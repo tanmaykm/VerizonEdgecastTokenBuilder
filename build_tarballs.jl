cbc315c6b303d9c4adf48e417742da9641ab184a
tan <tanmaykm@gmail.com>
2019-12-03 13:22:55 +0530

    fix travis build

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,10 +2,10 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "ectoken"
+name = "VerizonEdgecastTokenBuilder"
 version = v"0.1.0"
 
-# Collection of sources required to build ectoken
+# Collection of sources required to build VerizonEdgecastTokenBuilder
 sources = [
     "https://github.com/VerizonDigital/ectoken.git" =>
     "7b8812d476f5be5b290fe2832859b9b7636f43ae",
@@ -20,7 +20,6 @@ export OPENSSL_INCLUDE="-I $WORKSPACE/srcdir/include"
 export OPENSSL_LIBS="$WORKSPACE/srcdir/lib/libssl.a $WORKSPACE/srcdir/lib/libcrypto.a"
 cd ectoken/c-ectoken/ecencrypt/
 gcc -m64 -O2 -Wall -Werror -std=gnu99 ec_encrypt.c ectoken_v3.c base64.c -o 64/ectoken3 $OPENSSL_LIBS $OPENSSL_INCLUDE -lm
-cp ectoken3 $WORKSPACE/destdir/bin/
 cp 64/ectoken3 $WORKSPACE/destdir/bin/
 exit
 
@@ -35,7 +34,7 @@ platforms = [
 
 # The products that we will ensure are always built
 products(prefix) = [
-    ExecutableProduct(prefix, "ectoken3", :ectoken3)
+    ExecutableProduct(prefix, "ectoken3", :ectoken)
 ]
 
 # Dependencies that must be installed before this package can be built
