b22e4327a01b622cc478b35ac3b45adf97e658f0
tan <tanmaykm@gmail.com>
2019-12-03 11:47:55 +0530

    Add autogenerated files to build ectoken

---
+++ README.md
@@ -0,0 +1,9 @@
+# VerizonEdgecastTokenBuilder
+
+[![Build Status](https://travis-ci.org/tanmaykm/VerizonEdgecastTokenBuilder.svg?branch=master)](https://travis-ci.org/tanmaykm/VerizonEdgecastTokenBuilder)
+
+This repository builds binary artifacts for the ectoken project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/tanmaykm/VerizonEdgecastTokenBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,48 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "ectoken"
+version = v"0.1.0"
+
+# Collection of sources required to build ectoken
+sources = [
+    "https://github.com/VerizonDigital/ectoken.git" =>
+    "7b8812d476f5be5b290fe2832859b9b7636f43ae",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+tar -xzf *.tar.gz
+export OPENSSL_INCLUDE="-I $WORKSPACE/srcdir/include"
+export OPENSSL_LIBS="$WORKSPACE/srcdir/lib/libssl.a $WORKSPACE/srcdir/lib/libcrypto.a"
+cd ectoken/c-ectoken/ecencrypt/
+gcc -m64 -O2 -Wall -Werror -std=gnu99 ec_encrypt.c ectoken_v3.c base64.c -o 64/ectoken3 $OPENSSL_LIBS $OPENSSL_INCLUDE -lm
+cp ectoken3 $WORKSPACE/destdir/bin/
+cp 64/ectoken3 $WORKSPACE/destdir/bin/
+exit
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:x86_64, libc=:musl),
+    Linux(:x86_64, libc=:glibc)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    ExecutableProduct(prefix, "ectoken3", :ectoken3)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/OpenSSL-v1.1.1%2Bc%2B0/build_OpenSSL.v1.1.1+c.jl"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
